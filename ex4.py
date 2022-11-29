ee7ca129d5e94b730921fb0e55385ddc960d503c
Azkoun <108796789+Azkoun@users.noreply.github.com>
2022-11-29 12:30:59 +0200

    Delete ex4.py

--- ex4.py
+++
@@ -1,5 +0,0 @@
-min = int(input("enter number of minutes"))
-days = min //60//24
-hours = min//60 %24
-mins = (min) % 60
-print(min , "minutes are" , days , "days," , hours,"hours, and", mins , "minutes"  )
